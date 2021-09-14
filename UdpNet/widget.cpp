6689f687706d6ae700864df67287aba04ed58967
HardToKnew <fengshaojun98@163.com>
2021-09-14 14:29:08 +0800

    更新UdpNet工程

--- UdpNet/widget.cpp
+++ UdpNet/widget.cpp
@@ -1,12 +1,22 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QMessageBox>
+#include <QStatusBar>
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
+Widget::Widget(QMainWindow *parent)
+    : QMainWindow(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    info_lable = new QLabel;
+    /*QStatusBar* status = new QStatusBar(this);
+    status->addWidget(info_lable);*/
+    info_lable->setText("send:    ");
+    this->statusBar()->addWidget(info_lable);
+
+    //ui->status
+    //ui->statusBar()->addWidget();
+
 
     udpSocket = new QUdpSocket(this);
     //udpSocket->bind(QHostAddress("192.168.0.250"),8080);
@@ -18,20 +28,52 @@ Widget::~Widget()
     delete ui;
 }
 
+int Widget::createRand()
+{
+    int m_rand;
+    do{
+        m_rand = rand();	// rand returns a number between 0 and 0x7FFF
+    } while(m_rand == 0);    // make sure m_rand is not zero
+    return m_rand;
 
+}
+
+int Widget::getRandPort()
+{
+    int m_Rand=0;
+    do
+    {
+        m_Rand=(rand() % (65535-49152+1))+49152;
+    }
+    while(m_Rand<49152||m_Rand>65535);
+    return m_Rand;
+
+}
 void Widget::on_openBt_clicked()
 {
-    //if (udpSocket->bind(ui->LocalPortEdit->text().toUInt()) == true){
-    if (udpSocket->bind(QHostAddress("192.168.0.25"),8080) == true){
-        QMessageBox::information(this,"提示","成功");
-    }else{
-        QMessageBox::information(this,"提示","失败");
+
+    if(!udpSocket->state())
+    {
+        QString qSlocalIP = ui->ipEdit_2->text();
+        QHostAddress localIP = QHostAddress(qSlocalIP);
+        quint16 port = getRandPort();
+        //if (udpSocket->bind(ui->LocalPortEdit->text().toUInt()) == true){
+        if (udpSocket->bind(QHostAddress(localIP),port) == true){
+            ui->ipEdit_2->setText(localIP.toString());
+            ui->ipEdit->setText(localIP.toString());
+            ui->LocalPortEdit->setText(QString::number(port));
+            QMessageBox::information(this,"提示","成功");
+        }
+        else
+        {
+            QMessageBox::information(this,"提示","失败");
+        }
     }
     //connect(udpSocket,SIGNAL(readyRead()),this,SLOT(realRead_slot()));
 }
 
 void Widget::realRead_slot(){
-    while (udpSocket->hasPendingDatagrams()) {
+    /*while (udpSocket->hasPendingDatagrams()) {
         QByteArray array;
         array.resize(udpSocket->pendingDatagramSize());
         udpSocket->readDatagram(array.data(),array.size());
@@ -39,6 +81,24 @@ void Widget::realRead_slot(){
         buf = array.data();
         //buf = QString(udpSocket->readAll());
         ui->recvEdit->appendPlainText(buf);
+    }*/
+    QHostAddress host;
+    quint16 port;
+    while(udpSocket->hasPendingDatagrams())
+    {
+        QByteArray datagram;
+            //char *buff = new char[1024];
+        datagram.resize(udpSocket->pendingDatagramSize());
+        int dataSize = udpSocket->readDatagram(datagram.data(),datagram.size(),&host,&port);
+        if (dataSize!=-1)
+        {
+            QString buf = datagram.data();
+            QString msg="[Receive from "+host.toString()+ ":" +QString::number(port)+"] Size: "+QString::number(dataSize)+"\n"+buf+"\n";
+            ui->recvEdit->appendPlainText(msg);
+            ui->ipEdit->setText(host.toString());
+            ui->DestPortEdit->setText(QString::number(port));
+        }
+            //delete [] buff;
     }
 }
 
@@ -53,6 +113,7 @@ void Widget::on_sendBt_clicked()
     address.setAddress(ui->ipEdit->text());
     sendbuf = ui->sendEdit->toPlainText();
     port = ui->DestPortEdit->text().toUInt();
+    info_lable->setText(QString("Send: "+QString::number(sendbuf.length())));
     udpSocket->writeDatagram(sendbuf.toUtf8().data(),sendbuf.length(),address,port);
 
 }
--- UdpNet/widget.h
+++ UdpNet/widget.h
@@ -2,19 +2,21 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QMainWindow>
 #include <QUdpSocket>
 #include <QString>
+#include <QLabel>
 #include <QHostAddress>
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
 
-class Widget : public QWidget
+class Widget : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    Widget(QWidget *parent = nullptr);
+    Widget(QMainWindow *parent = nullptr);
     ~Widget();
 
     QUdpSocket *udpSocket;
@@ -31,5 +33,9 @@ private slots:
 
 private:
     Ui::Widget *ui;
+    QLabel *info_lable;
+
+    int createRand();
+    int getRandPort();
 };
 #endif // WIDGET_H
